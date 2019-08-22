2628ec8454803e2c1e00ec0d06bec6d88ac56ffa
hjw1258 <897074692@qq.com>
2019-08-22 15:11:50 +0800

    create project

---
+++ src/main/java/com/newer/dome0823/Dome0823Application.java
@@ -0,0 +1,13 @@
+package com.newer.dome0823;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dome0823Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dome0823Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newer/dome0823/Dome0823ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newer.dome0823;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dome0823ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
